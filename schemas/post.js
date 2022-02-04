7a11e819ccd9aa5b76c460ae27f103af69fd9e1e
Teerut26 <teerutsr@gmail.com>
2022-02-05 01:04:34 +0700

    🔥add announcement

--- schemas/post.js
+++ schemas/post.js
@@ -1,65 +1,73 @@
+import { v4 as uuidv4 } from 'uuid';
 export default {
-  name: 'post',
-  title: 'Post',
-  type: 'document',
+  name: "post",
+  title: "Post",
+  type: "document",
+  // initialValue: () => ({
+  //   publishedAt: (new Date()).toISOString()
+  // }),
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: Rule =>  Rule.required()
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
+      name: "slug",
+      title: "Slug",
+      type: "string",
+      initialValue: (String(uuidv4())),
     },
     {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
+      validation: Rule =>  Rule.required()
     },
     {
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
       options: {
         hotspot: true,
       },
+      validation: Rule =>  Rule.required()
     },
     {
-      name: 'category',
-      title: 'Category',
-      type: 'reference',
-      to: {type: 'category'},
+      name: "category",
+      title: "Category",
+      type: "reference",
+      to: { type: "category" },
+      validation: Rule =>  Rule.required()
     },
     {
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+      initialValue: (new Date()).toISOString(),
+      validation: Rule =>  Rule.required()
     },
     {
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+      validation: Rule =>  Rule.required()
     },
   ],
 
   preview: {
     select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
     },
     prepare(selection) {
-      const {author} = selection
+      const { author } = selection;
       return Object.assign({}, selection, {
         subtitle: author && `by ${author}`,
-      })
+      });
     },
   },
-}
+};
